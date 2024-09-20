9a860288757265c1c4c92c4477b60734e442e5cd
Grinch <grinch9718@gmail.com>
2024-09-20 02:38:24 +0000

    Pending changes exported from your codespace

---
+++ portfolio-with-nextjs/app/create-todo/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from "react";
+
+function CreateTodo() {
+
+    return(
+        <div>
+            <h1>Create Todo Works</h1>
+        </div>
+    )
+}
+
+export default CreateTodo
---
+++ portfolio-with-nextjs/app/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+import Link from "next/link";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <h1>Home Works</h1>
+  );
+}
